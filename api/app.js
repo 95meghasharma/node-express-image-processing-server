61828d4b277db628978c2408bf4374b77728d6ff
Megha Sharma <95meghasharma19@gmail.com>
2021-01-13 15:56:06 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
-var express = require('express');
+const express = require('express');
 const path = require('path');
+
 const app = express();
-const pathToIndex = path.resolve('D:/amway-renewal/node-express-image-processing-server/client','./index.html');
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
 });
-module.exports=app;
+
+module.exports = app;
