82e26b7dfbc3622fe04a7be67f692d25c12155ab
Megha Sharma <95meghasharma19@gmail.com>
2021-01-13 14:53:32 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 const pathToIndex = path.resolve('./../client/photos/index.html');
 app.use('/*', function (req, res) {
-  res.sendFile(pathToIndex);
+  res.sendFile(pathToIndexgit config --global user.email "youremail@yourdomain.com");
 })
 module.exports(app);
